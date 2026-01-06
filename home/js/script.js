967d7382b3cefc00ac7a85dd5484c417ff10b151
Sheryn Mae Abril <sgabril@student.apc.edu.ph>
2026-01-06 07:25:39 +0300

    Input divider and edited html

--- home/js/script.js
+++ home/js/script.js
@@ -21,3 +21,32 @@ document.querySelectorAll('nav a').forEach(link => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const text = [
+  "Computer Science Student",
+  "Cyber Security Enthusiast",
+  "Future Digital Explorer 🌍"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === text.length) count = 0;
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    setTimeout(() => {
+      index = 0;
+      count++;
+    }, 2000);
+  }
+
+  setTimeout(type, 120);
+})();
